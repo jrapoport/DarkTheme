8f15725484eb8f0151bc9833f258363f3cbee3ac
Jacob Rapoport <jacob@rumblemonkey.com>
2019-10-22 12:14:43 -0700

    added autoloading; some errors

--- Editor/DarkTheme.cs
+++ Editor/DarkTheme.cs
@@ -1,4 +1,5 @@
-﻿// --- instance.id ------------------------------------------------------------
+﻿// Based on: https://gist.github.com/instance-id/3046ad29bd8782a3b3398cd9ae48ca92
+// --- instance.id ------------------------------------------------------------
 // Thanks to TheZombieKiller and Peter77 for creating this
 // https://forum.unity.com/threads/editor-skinning-thread.711059/#post-4785434
 // Tested on Unity 2019.3.0b1 - 95% Dark mode Conversion
@@ -7,73 +8,101 @@
 // ----------------------------------------------------------------------------
 
 using System;
+using System.IO;
 using System.Text.RegularExpressions;
-using UnityEngine;
-using UnityEngine.UIElements;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditorInternal;
- 
-public static class DarkTheme
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Editor
 {
-    [MenuItem("Theme/Init Dark")]
-    static void Init()
+    [InitializeOnLoad]
+    public static class DarkTheme
     {
-        foreach (var sheet in Resources.FindObjectsOfTypeAll<StyleSheet>())
+        static DarkTheme()
         {
-            if (ContainsInsensitive(sheet.name, "Dark"))
-                continue;
- 
-            if (!ContainsInsensitive(sheet.name, "Light"))
+            InitAsync();
+        }
+
+        private static async void InitAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(0.1f));
+            var darkMarker = Path.Combine(Application.persistentDataPath, "dark");
+            if (File.Exists(darkMarker))
             {
-                InvertColors(sheet);
-                continue;
+                File.Delete(darkMarker);
+                return;
             }
- 
-            var dark = null as StyleSheet;
-            var path = ReplaceInsensitive(AssetDatabase.GetAssetPath(sheet), "Light", "Dark");
-            var name = ReplaceInsensitive(sheet.name, "Light", "Dark");
- 
-            if (path == "Library/unity editor resources")
-                dark = EditorGUIUtility.Load(name) as StyleSheet;
-            else
-                dark = AssetDatabase.LoadAssetAtPath<StyleSheet>(path);
- 
-            if (!dark)
-                InvertColors(sheet);
-            else
+            using(var sw = new StreamWriter(darkMarker))
             {
-                string oldName = sheet.name;
-                EditorUtility.CopySerialized(dark, sheet);
-                sheet.name = oldName;
+                sw.Write(true);
             }
+            Init();
+        }
+    
+        [MenuItem("Theme/Init Dark")]
+        private static void Init()
+        {
+            foreach (var sheet in Resources.FindObjectsOfTypeAll<StyleSheet>())
+            {
+                if (ContainsInsensitive(sheet.name, "Dark"))
+                    continue;
+ 
+                if (!ContainsInsensitive(sheet.name, "Light"))
+                {
+                    InvertColors(sheet);
+                    continue;
+                }
+            
+                StyleSheet dark;
+                var path = ReplaceInsensitive(AssetDatabase.GetAssetPath(sheet), "Light", "Dark");
+                var name = ReplaceInsensitive(sheet.name, "Light", "Dark");
+ 
+                if (path == "Library/unity editor resources")
+                    dark = EditorGUIUtility.Load(name) as StyleSheet;
+                else
+                    dark = AssetDatabase.LoadAssetAtPath<StyleSheet>(path);
+            
+                if (!dark)
+                    InvertColors(sheet);
+                else
+                {
+                    string oldName = sheet.name;
+                    EditorUtility.CopySerialized(dark, sheet);
+                    sheet.name = oldName;
+                }
+            }
+            EditorUtility.RequestScriptReload();
+            InternalEditorUtility.RepaintAllViews();
         }
  
-        EditorUtility.RequestScriptReload();
-        InternalEditorUtility.RepaintAllViews();
-    }
- 
-    static void InvertColors(StyleSheet sheet)
-    {
-        var serialized = new SerializedObject(sheet); serialized.Update();
-        var colors     = serialized.FindProperty("colors");
+        static void InvertColors(StyleSheet sheet)
+        {
+            var serialized = new SerializedObject(sheet); serialized.Update();
+            var colors     = serialized.FindProperty("colors");
      
-        for (int i = 0; i < colors.arraySize; i++)
+            for (int i = 0; i < colors.arraySize; i++)
+            {
+                var property = colors.GetArrayElementAtIndex(i);
+                Color.RGBToHSV(property.colorValue, out var h, out var s, out var v);
+                property.colorValue = Color.HSVToRGB(h, s, 1 - v);
+            }
+ 
+            serialized.ApplyModifiedProperties();
+        }
+    
+        static string ReplaceInsensitive(string str, string oldValue, string newValue)
         {
-            var property = colors.GetArrayElementAtIndex(i);
-            Color.RGBToHSV(property.colorValue, out var h, out var s, out var v);
-            property.colorValue = Color.HSVToRGB(h, s, 1 - v);
+            return Regex.Replace(str, 
+                Regex.Escape(oldValue), 
+                newValue.Replace("$", "$$"), RegexOptions.IgnoreCase);
         }
  
-        serialized.ApplyModifiedProperties();
-    }
- 
-    static string ReplaceInsensitive(string str, string oldValue, string newValue)
-    {
-        return Regex.Replace(str, Regex.Escape(oldValue), newValue.Replace("$", "$$"), RegexOptions.IgnoreCase);
-    }
- 
-    static bool ContainsInsensitive(string str, string find)
-    {
-        return str.IndexOf(find, StringComparison.OrdinalIgnoreCase) != -1;
+        static bool ContainsInsensitive(string str, string find)
+        {
+            return str.IndexOf(find, StringComparison.OrdinalIgnoreCase) != -1;
+        }
     }
 }
