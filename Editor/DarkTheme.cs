94618bf9d1b6c175ea2419b62bb4e12157c65fad
Jacob Rapoport <jacob@rumblemonkey.com>
2019-10-22 13:14:47 -0700

    fixed

--- Editor/DarkTheme.cs
+++ Editor/DarkTheme.cs
@@ -12,97 +12,107 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnityEditor;
+using UnityEditor.Callbacks;
 using UnityEditorInternal;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
-namespace Editor
+[InitializeOnLoad]
+public static class DarkTheme
 {
-    [InitializeOnLoad]
-    public static class DarkTheme
+    static string darkMarker = Path.Combine(Application.persistentDataPath, "dark.theme");
+
+    static DarkTheme()
     {
-        static DarkTheme()
+        InitAsync();
+    }
+
+    [DidReloadScripts]
+    private static void OnScriptsReloaded()
+    {
+        if (File.Exists(darkMarker))
+        { 
+            ApplyTheme();
+        }
+    }
+    private static void InitAsync()
+    {
+        // await Task.Delay(TimeSpan.FromSeconds(0.1f));
+        if (File.Exists(darkMarker))
         {
-            InitAsync();
+            File.Delete(darkMarker);
+            return;
+        }
+        using(var sw = new StreamWriter(darkMarker))
+        {
+            sw.Write(true);
+        }
+        EditorUtility.RequestScriptReload();
+    }
+
+    [MenuItem("Theme/Init Dark")]
+    private static async void ApplyTheme()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(0.1f));
+
+        foreach (var sheet in Resources.FindObjectsOfTypeAll<StyleSheet>())
+        {
+            if (ContainsInsensitive(sheet.name, "Dark"))
+                continue;
+
+            if (!ContainsInsensitive(sheet.name, "Light"))
+            {
+                InvertColors(sheet);
+                continue;
+            }
+        
+            StyleSheet dark;
+            var path = ReplaceInsensitive(AssetDatabase.GetAssetPath(sheet), "Light", "Dark");
+            var name = ReplaceInsensitive(sheet.name, "Light", "Dark");
+
+            if (path == "Library/unity editor resources")
+                dark = EditorGUIUtility.Load(name) as StyleSheet;
+            else
+                dark = AssetDatabase.LoadAssetAtPath<StyleSheet>(path);
+        
+            if (!dark)
+                InvertColors(sheet);
+            else
+            {
+                string oldName = sheet.name;
+                EditorUtility.CopySerialized(dark, sheet);
+                sheet.name = oldName;
+            }
+        }
+        EditorUtility.RequestScriptReload();
+        InternalEditorUtility.RepaintAllViews();
+    }
+
+    static void InvertColors(StyleSheet sheet)
+    {
+        var serialized = new SerializedObject(sheet); serialized.Update();
+        var colors     = serialized.FindProperty("colors");
+ 
+        for (int i = 0; i < colors.arraySize; i++)
+        {
+            var property = colors.GetArrayElementAtIndex(i);
+            Color.RGBToHSV(property.colorValue, out var h, out var s, out var v);
+            property.colorValue = Color.HSVToRGB(h, s, 1 - v);
         }
 
-        private static async void InitAsync()
-        {
-            await Task.Delay(TimeSpan.FromSeconds(0.1f));
-            var darkMarker = Path.Combine(Application.persistentDataPath, "dark");
-            if (File.Exists(darkMarker))
-            {
-                File.Delete(darkMarker);
-                return;
-            }
-            using(var sw = new StreamWriter(darkMarker))
-            {
-                sw.Write(true);
-            }
-            Init();
-        }
-    
-        [MenuItem("Theme/Init Dark")]
-        private static void Init()
-        {
-            foreach (var sheet in Resources.FindObjectsOfTypeAll<StyleSheet>())
-            {
-                if (ContainsInsensitive(sheet.name, "Dark"))
-                    continue;
- 
-                if (!ContainsInsensitive(sheet.name, "Light"))
-                {
-                    InvertColors(sheet);
-                    continue;
-                }
-            
-                StyleSheet dark;
-                var path = ReplaceInsensitive(AssetDatabase.GetAssetPath(sheet), "Light", "Dark");
-                var name = ReplaceInsensitive(sheet.name, "Light", "Dark");
- 
-                if (path == "Library/unity editor resources")
-                    dark = EditorGUIUtility.Load(name) as StyleSheet;
-                else
-                    dark = AssetDatabase.LoadAssetAtPath<StyleSheet>(path);
-            
-                if (!dark)
-                    InvertColors(sheet);
-                else
-                {
-                    string oldName = sheet.name;
-                    EditorUtility.CopySerialized(dark, sheet);
-                    sheet.name = oldName;
-                }
-            }
-            EditorUtility.RequestScriptReload();
-            InternalEditorUtility.RepaintAllViews();
-        }
- 
-        static void InvertColors(StyleSheet sheet)
-        {
-            var serialized = new SerializedObject(sheet); serialized.Update();
-            var colors     = serialized.FindProperty("colors");
-     
-            for (int i = 0; i < colors.arraySize; i++)
-            {
-                var property = colors.GetArrayElementAtIndex(i);
-                Color.RGBToHSV(property.colorValue, out var h, out var s, out var v);
-                property.colorValue = Color.HSVToRGB(h, s, 1 - v);
-            }
- 
-            serialized.ApplyModifiedProperties();
-        }
-    
-        static string ReplaceInsensitive(string str, string oldValue, string newValue)
-        {
-            return Regex.Replace(str, 
-                Regex.Escape(oldValue), 
-                newValue.Replace("$", "$$"), RegexOptions.IgnoreCase);
-        }
- 
-        static bool ContainsInsensitive(string str, string find)
-        {
-            return str.IndexOf(find, StringComparison.OrdinalIgnoreCase) != -1;
-        }
+        serialized.ApplyModifiedProperties();
+    }
+
+    static string ReplaceInsensitive(string str, string oldValue, string newValue)
+    {
+        return Regex.Replace(str, 
+            Regex.Escape(oldValue), 
+            newValue.Replace("$", "$$"), RegexOptions.IgnoreCase);
+    }
+
+    static bool ContainsInsensitive(string str, string find)
+    {
+        return str.IndexOf(find, StringComparison.OrdinalIgnoreCase) != -1;
     }
 }
